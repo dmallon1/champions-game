05e0b06752834f833493dbff4c038ffc4bd1fcd9
Dan Mallon <d.mallon@me.com>
2021-12-12 19:56:52 -0500

    progress

--- contracts/ChampionGame.sol
+++ contracts/ChampionGame.sol
@@ -6,12 +6,15 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "./ChampionCoin.sol";
 
 contract ChampionGame is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     // number of tokens have been minted so far
     uint16 public minted;
+    uint256 public constant DAILY_CCOIN_RATE = 10000 ether;
+    uint256 public constant MINIMUM_TO_EXIT = 2 days;
 
     uint256 public totalSupply = 1000000;
     address public admin;
@@ -23,6 +26,8 @@ contract ChampionGame is ERC721URIStorage, Ownable {
         LEGENDARY
     }
     mapping(uint256 => Champion) public champions; // id to champion stats
+    mapping(uint256 => DungeonStake) public dungeon; // keeping track of tokens in dungeon
+
     mapping(address => bool) public auth;
     struct Champion {
         uint8 head;
@@ -31,18 +36,22 @@ contract ChampionGame is ERC721URIStorage, Ownable {
         uint8 offhand;
         Level level;
     }
-    bytes32 internal entropySauce;
 
-    constructor() ERC721("Champions Game", "CGAME") {
-        // you'd need to add address of the champions ecr20 coin
-        // admin = msg.sender; // ownable does this automatically
+    struct DungeonStake {
+        address owner;
+        uint16 tokenId;
+        uint80 value;
     }
 
-    function mint(address recipient, string memory tokenURI)
-        public
-        noCheaters
-        returns (uint256)
-    {
+    bytes32 internal entropySauce;
+
+    ChampionCoin championCoin;
+
+    constructor(address _ccoin) ERC721("Champions Game", "CGAME") {
+        championCoin = ChampionCoin(_ccoin);
+    }
+
+    function mint(address recipient) external noCheaters returns (uint256) {
         // validation
         // does it cost anything? -> do you have enough?
         // make sure there is still supply
@@ -61,9 +70,41 @@ contract ChampionGame is ERC721URIStorage, Ownable {
             level: Level.COMMON
         });
 
-        // _setTokenURI(newItemId, tokenURI);
-
         return newItemId;
+    }
+
+    function goToDungeon(uint256 championId) external {
+        require(msg.sender == ownerOf(championId));
+
+        transferFrom(msg.sender, address(this), championId);
+
+        dungeon[championId] = DungeonStake({
+            owner: msg.sender,
+            tokenId: uint16(championId),
+            value: uint80(block.timestamp)
+        });
+    }
+
+    function claimRewards(uint256 championId, bool unstake) external {
+        DungeonStake memory dungeonStake = dungeon[championId];
+
+        require(dungeonStake.owner == msg.sender);
+        require(
+            block.timestamp - dungeonStake.value < MINIMUM_TO_EXIT,
+            "not enough time has passed to claim rewards"
+        );
+
+        uint256 owed = (block.timestamp - dungeonStake.value) *
+            DAILY_CCOIN_RATE;
+
+        championCoin.mint(msg.sender, owed);
+
+        if (unstake) {
+            transferFrom(address(this), msg.sender, championId);
+            delete dungeon[championId];
+        } else {
+            dungeon[championId].value = uint80(block.timestamp);
+        }
     }
 
     function _rand() internal view returns (uint256) {
--- test/Token.js
+++ test/Token.js
@@ -1,21 +1,27 @@
 const { expect } = require("chai");
 
-describe("Token contract", function () {
-    it("Deployment should assign the total supply of tokens to the owner", async function () {
+describe("Token contract", () => {
+    it("Deployment should assign the total supply of tokens to the owner", async () => {
         const [owner] = await ethers.getSigners();
 
-        const Token = await ethers.getContractFactory("ChampionGame");
+        const championsCoin = await ethers.getContractFactory("ChampionCoin");
+        const coinContract = await championsCoin.deploy();
 
-        const hardhatToken = await Token.deploy();
+        const championsGame = await ethers.getContractFactory("ChampionGame");
 
-        const result = await hardhatToken.mint(owner.address, "");
-        console.log(await hardhatToken.mint(owner.address, ""));
-        console.log(await hardhatToken.mint(owner.address, ""));
-        console.log(await hardhatToken.mint(owner.address, ""));
+        const hardhatToken = await championsGame.deploy(coinContract.address);
+
+        const result = await hardhatToken.mint(owner.address);
+        console.log(await hardhatToken.mint(owner.address));
+        console.log(await hardhatToken.mint(owner.address));
+        console.log(await hardhatToken.mint(owner.address));
         console.log(result);
 
         // const ownerBalance = await hardhatToken.balanceOf(owner.address);
         // console.log(ownerBalance);
         console.log(await hardhatToken.minted())
+        await hardhatToken.goToDungeon(3)
+        // hardhatToken.goToDungeon(3)
+        // console.log(await hardhatToken.dungeon(3));
     });
 });
